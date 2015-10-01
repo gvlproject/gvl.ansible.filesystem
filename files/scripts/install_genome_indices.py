bc08cb2685c4c2ef9e27361871c140589a8cfb8e
nuwan_ag <nuwan.ag@gmail.com>
2015-10-01 21:41:22 +1000

    Made list of data managers to run configurable on a per genome basis

--- files/scripts/install_genome_indices.py
+++ files/scripts/install_genome_indices.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-import datetime as dt
+import argparse
 import logging
 import time
-import yaml
-import argparse
 
 from bioblend.galaxy import GalaxyInstance
 from bioblend.galaxy.client import ConnectionError
+import datetime as dt
+import yaml
+
 
 # Omit (most of the) logging by external libraries
 logging.getLogger('bioblend').setLevel(logging.ERROR)
@@ -15,10 +16,13 @@ logging.getLogger('requests').setLevel(logging.ERROR)
 
 DEFAULT_GALAXY_URL = "http://localhost:8080/"
 
+
 class JobFailedException(Exception):
     pass
 
+
 class ProgressConsoleHandler(logging.StreamHandler):
+
     """
     A handler class which allows the cursor to stay on
     one line for selected messages
@@ -54,6 +58,7 @@ def load_indices_list(indices_file):
         indices_list = yaml.load(f)
     return indices_list
 
+
 def wait_for_data_manager_jobs(galaxy_ref, jobs):
     # Monitor the job(s)
     log.debug("\tJob running", extra={'same_line': True})
@@ -69,17 +74,19 @@ def wait_for_data_manager_jobs(galaxy_ref, jobs):
             log.debug("", extra={'same_line': True})
             time.sleep(10)
 
+
 def run_data_manager(galaxy_ref, dbkey_name, dm_tool, tool_input):
     response = galaxy_ref.tools.run_tool('', dm_tool, tool_input)
     jobs = response.get('jobs', [])
     # Check if a job is actually running
     if len(jobs) == 0:
         raise JobFailedException("\t(!) No '{0}' job found for '{1}'".format(dm_tool,
-                    dbkey_name))
+                                                                             dbkey_name))
     else:
         wait_for_data_manager_jobs(galaxy_ref, jobs)
 
-def install_genome(galaxy_ref, genome, data_manager_list):
+
+def install_genome(galaxy_ref, genome):
     """
     Runs all data managers provided to setup and install the specified genome.
 
@@ -87,42 +94,47 @@ def install_genome(galaxy_ref, genome, data_manager_list):
     :param galaxy_ref: The Galaxy instance on which to install the genome
 
     :type genome: This is an individual dictionary element of the dbkeys array from the indices file
-    :param genome: A dictionary containing information about an individual genome to install
-
-    :type data_manager_list: A list of data managers from the indices file
-    :param data_manager_list: All data managers in this list will be installed
+    :param genome: A dictionary containing information about an individual genome to install. Two attributes are
+                treated specially: dbkey and data_managers. The dbkey is used as the identifier for a genome and
+                the data_managers is a list of data_managers to run for this genome.
     """
     errored_dms = []
     dbkey_name = genome.get('dbkey')
-    for idx, dm in enumerate(data_manager_list):
+    for idx, dm in enumerate(genome.get('data_managers')):
         dm_tool = dm.get('id')
         # Initate tool installation
         log.debug('[DM: {0}/{1}] Installing genome {2} with '
                   'Data Manager: {3}'.format(idx,
-                                  len(data_manager_list), dbkey_name, dm_tool))
+                                             len(genome.get('data_managers')), dbkey_name, dm_tool))
         tool_input = genome
         start = dt.datetime.now()
         try:
             run_data_manager(galaxy_ref, dbkey_name, dm_tool, tool_input)
             log.debug("\tDbkey '{0}' installed successfully in '{1}'".format(
-                  genome.get('dbkey'), dt.datetime.now() - start))
-        except ConnectionError, e:
+                genome.get('dbkey'), dt.datetime.now() - start))
+        except ConnectionError as e:
             response = None
             log.error("\t* Error installing genome {0} for DM {1} (after {2}): {3}"
                       .format(dbkey_name, dm_tool, dt.datetime.now() - start, e.body))
             errored_dms.append({'dbkey': dbkey_name, 'DM': dm_tool})
     return errored_dms
 
+
 def install_genomes(galaxy_url, api_key, indices_file):
     istart = dt.datetime.now()
-    galaxy_ref = GalaxyInstance(galaxy_url or indices_list['galaxy_instance'], api_key or indices_list['api_key'])
+    galaxy_ref = GalaxyInstance(
+        galaxy_url or indices_list['galaxy_instance'],
+        api_key or indices_list['api_key'])
     indices_list = load_indices_list(indices_file)
     for idx, genome in enumerate(indices_list['genomes']):
-        log.debug('Processing {0} of {1} genomes - name: {2}'.format(idx, len(indices_list['genomes']), genome['dbkey']))
-        errored_dms = install_genome(galaxy_ref, genome, indices_list['data_managers'])
+        log.debug('Processing {0} of {1} genomes - name: {2}'.format(idx,
+                                                                     len(indices_list['genomes']),
+                                                                     genome['dbkey']))
+        errored_dms = install_genome(galaxy_ref, genome)
     log.info("All genomes & DMs listed in '{0}' have been processed.".format(indices_file))
     log.info("Errored DMs: {0}".format(errored_dms))
     log.info("Total run time: {0}".format(dt.datetime.now() - istart))
+
 
 def _setup_logging():
     formatter = logging.Formatter('%(asctime)s %(levelname)-5s - %(message)s')
@@ -131,7 +143,7 @@ def _setup_logging():
     console = logging.StreamHandler()
     console.setFormatter(formatter)
 
-    logger = logging.getLogger( __name__ )
+    logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(progress)
     logger.addHandler(file_handler)
@@ -141,9 +153,24 @@ if __name__ == "__main__":
     global log
     log = _setup_logging()
     parser = argparse.ArgumentParser(description="usage: python %prog [options]")
-    parser.add_argument("-g", "--galaxy", default=DEFAULT_GALAXY_URL, help="URL of galaxy server to use. The default is %s" % DEFAULT_GALAXY_URL)
-    parser.add_argument("-a", "--api_key", type=str, help="Galaxy admin user API key", required=True)
-    parser.add_argument("-i", "--indices_file", type=str, help="Reference genomes to install (see genome_list.yaml.sample)", required=True)
+    parser.add_argument(
+        "-g",
+        "--galaxy",
+        default=DEFAULT_GALAXY_URL,
+        help="URL of galaxy server to use. The default is %s" %
+        DEFAULT_GALAXY_URL)
+    parser.add_argument(
+        "-a",
+        "--api_key",
+        type=str,
+        help="Galaxy admin user API key",
+        required=True)
+    parser.add_argument(
+        "-i",
+        "--indices_file",
+        type=str,
+        help="Reference genomes to install (see genome_list.yaml.sample)",
+        required=True)
     args = parser.parse_args()
 
     install_genomes(args.galaxy, args.api_key, args.indices_file)
